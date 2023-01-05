1a49592e9c970da5a515ac0af4c10063c0c171cc
OmarNashat01 <omarnashat01@gmail.com>
2023-01-05 23:16:10 +0200

    add music bot

---
+++ Simple-Python-Discord-Music-Bot-main/Main.py
@@ -0,0 +1,40 @@
+import nextcord as discord
+from nextcord.enums import Status 
+from nextcord.ext import commands
+import os
+import json
+from nextcord import File, ButtonStyle, Embed, Color, SelectOption, Intents, Interaction, SlashOption, Member
+
+with open('settings.json', mode='r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+intents = Intents.default()
+intents.message_content = True
+intents.members = True
+client = commands.Bot(command_prefix="botaya ", intents=intents)
+
+@client.event 
+async def on_ready(): 
+    await client.change_presence(status = discord.Status.online, activity = discord.Activity(type = 2, name = "Music Bot")) 
+    print("Ready!")
+    
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'Load {extension} !')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'Unload {extension} !')
+
+@client.command()
+async def reload(ctx, extension): 
+    client.reload_extension(f'cogs.{extension}')
+    await ctx.send(f'Reload {extension} !')
+
+for filename in os.listdir('./cogs'): 
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
+client.run(jdata['TOKEN']) #Go to settings.json set your Discord bot token
--- bot.py
+++ bot.py
@@ -19,6 +19,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
+### Youtube muscic player
+
 # engine = pyttsx3.init('sapi5')
 # voices = engine.getProperty('voices')
 # engine.setProperty('voice', voices[1].id)
